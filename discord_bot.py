5de8039589f5160741b4777a866f90827870fa63
sem <34904@ma-web.nl>
2022-10-26 18:53:30 +0200

    hello hi

--- discord_bot.py
+++ discord_bot.py
@@ -9,3 +9,16 @@ async def on_ready():
     guild = client.guilds[0]
     print(guild.name, "is the name of the server")
     print(client.user, "has connected to the client")
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    await channel.send("I'm online!")
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author,"is the user who wrote the message")
+    print(message.created_at,"is when the message was posted")
+    print(message.channel,"is the channel this message was posted in")
+    await message.channel.send("Hello " + message.author)
+    if message.author.bot == False:
+        message.channel.send
